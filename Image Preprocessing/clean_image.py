9b770da826c64634f940079dcd8ff8132a4b218c
weijunsyu <weijunsyu@gmail.com>
2022-02-20 12:39:00 -0800

    Update clean_image.py

--- Image Preprocessing/clean_image.py
+++ Image Preprocessing/clean_image.py
@@ -182,8 +182,7 @@ def exportfilelist(fileList, name, path):
 
     file.close()
 
-
-if __name__ == "__main__":
+def main():
     parser = argparse.ArgumentParser(description="Do preprocessing on image data for supervised learning tasks. Take image files from source directories and perform preprocessing operation on each image. The directory tree determines the data labels such that each folder name is a label for all images held within itself and its subdirectories. Whitespace in labels will be replaced with underscores. Export each image as a flat file to the target directory along with a single metadata file 'metadata.csv' holding the labels, image shape data, value format of flattened image, and the path to the flattened image file. Images that are unlabeled will have their paths exported to a file 'unlabeled.csv' in the same directory as the metadata file. Images that contain useless data will have their paths exported to a file 'useless.csv' in the same directory as the metadata file.")
     parser.add_argument("target", type=str, help="Path to the target directory where all processed data output files will be stored.")
     parser.add_argument("-m", "--metadata", type=str, help="Optional path to the metadata file. If unset then metadata file will be stored in the target directory.")
@@ -281,3 +280,7 @@ if __name__ == "__main__":
 
     if not args.quiet:
         print("Finished operation.")
+
+
+if __name__ == "__main__":
+    main()
