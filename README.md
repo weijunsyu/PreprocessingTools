abe838d4d02394a99c97a9b88f9242739ffd5178
WeiJun Syu <weijunsyu@gmail.com>
2022-02-24 16:51:26 -0800

    update

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
 # Preprocessing Tools
-Python scripts for pre-processing data
+Python scripts for pre-processing data.
+Each folder is self contained.
 
 ## Text Preprocessing:
 1. **Text Cleaning: _clean_text.py_**
     - Under construction.
 
 ## Image Preprocessing:
-1. **Image Cleaning: _clean_image.py_**
+1. **Image Testing:**
+    - Do testing of dataset to determine number of labels, number of images, and metadata of each image (size, resolution, number of colour channels, etc)
+    - Under construction
+2. **Image Cleaning: _clean_image.py_**
     - Do initial preprocessing on image data for supervised learning tasks. Preprocessing includes removing useless data, and conversion to stardardized format. Export as rgb or greyscale.
     - Take image files from source directories and perform preprocessing operation on each image. The directory tree determines the data labels such that each folder name is a label for all images held within itself and its subdirectories. Whitespace in labels will be replaced with underscores.
     - Export each image as a flat file to the target directory along with a single metadata file 'metadata.csv' holding the labels, image shape data, value format of flattened image, and the path to the flattened image file.
--- Text_Preprocessing/clean_text.py
+++ Text_Preprocessing/clean_text.py
@@ -2,110 +2,260 @@ import numpy as np
 import os
 import sys
 import argparse
+import string
 from pdfminer.pdfparser import PDFParser
 
 
-def readpdf(file):
+METADATA_FILENAME = "metadata.csv"
+UNLABELED_FILENAME = "unlabeled.csv"
+USELESS_FILENAME = "useless.csv"
+FLAT_TEXT_EXT = ".csv"
+
+
+def removeaffix(string, prefix=None, suffix=None):
+    if prefix:
+        try:
+            string = string.removeprefix(prefix)
+        except:
+            if string.startswith(prefix):
+                string = string[len(prefix):]
+    if suffix:
+        try:
+            string = string.removesuffix(suffix)
+        except:
+            if string.endswith(suffix):
+                string = string[:-len(suffix)]
+    return string
+
+def trimpathsep(path, leading=True, trailing=True):
+    if leading and trailing:
+        return path.strip(os.sep)
+    elif leading:
+        return path.lstrip(os.sep)
+    elif trailing:
+        return path.rstrip(os.sep)
+
+def checkdirs(dirs, label="", verbose=False, quiet=False):
+    invalid = False
+    for dir in dirs:
+        invalid = checkdir(dir, label=label, verbose=verbose, quiet=quiet)
+        if invalid and not verbose:
+            return invalid
+    return invalid
+
+def checkdir(directory, label="", verbose=False, quiet=False):
+    if not os.path.isdir(directory):
+        if verbose:
+            print("The " + label + " directory: '" + dir + "'" + " does not exist.")
+        elif not quiet:
+            print("Some " + label + " path(s) are invalid.")
+        return True
+    return False
+
+def modfilename(path, prefix="", suffix=""):
+    root, file = os.path.split(path)
+    file, ext = os.path.splitext(file)
+    file = prefix + file + suffix
+    return os.path.join(root, file + ext)
+
+def getlabels(path, delimiter=os.sep, verbose=False):
+    labels = path.split(delimiter)
+    if verbose:
+        if labels[0]:
+            print("The labels for text are: " + str(labels))
+    return labels
+
+def getmeta(text, verbose=False):
     pass
 
-def readflat(filepath):
-    with open(filepath, "r") as file:
-        for line
+def checkuseful(meta, minchars=0, maxchars=0, verbose=False):
+    pass
+
+def savetext(text, filename, directory, compress=False, abspath=False):
+    ext = FLAT_TEXT_EXT
+    if compress:
+        ext += ".gz"
+    filepath = os.path.join(directory, filename + ext)
+    if abspath:
+        filepath = os.path.abspath(filepath)
+
+    np.savetxt(filepath, text)
+    return filepath
+
+def exportmeta(data, path, float=False):
+    filepath = iteratefilename(os.path.join(path, METADATA_FILENAME), prefix="_")
+    try:
+        file = open(filepath, 'x')
+    except:
+        print("SOMETHING IS WRONG THIS SHOULD NEVER BE REACHED.")
+        sys.exit()
+    for text in data:
+        labels, textmeta, textpath = text
+        # labels: num labels followed by each label
+        file.write(str(len(labels)) + " ")
+        for label in labels:
+            label = "_".join(label.split())
+            file.write(str(label) + " ")
+        # text metadata [num charters, num words, ]
+
+        # path to the flattened text file (.csv)
+        file.write(textpath + "\n")
+    file.close()
+
+def createdir(directory, label='', clean=False, verbose=False, quiet=False):
+    if not os.path.isdir(directory):
+        if not quiet:
+            print("The " + label + " directory '" + directory + "' does not exist. Creating directory...")
+        try:
+            os.makedirs(directory)
+            if verbose:
+                print("Created " + label + " directory: " + "'" + directory + "'")
+        except OSError as error:
+            if not quiet:
+                print("Failed to create " + label + " directory: '" + directory + "'")
+            return True
+    else:
+        if not quiet:
+            print("The " + label + " directory '" + directory + "' already exists.")
+        if clean:
+            return True
+    return False
+
+def iteratefilename(path, initial=0, prefix="_", suffix="", prepend=False):
+    newpath = path
+    while os.path.isfile(newpath):
+        if prepend:
+            newpath = modfilename(path, prefix=prefix+str(initial)+suffix)
+        else:
+            newpath = modfilename(path, suffix=prefix+str(initial)+suffix)
+        initial += 1
+    return newpath
+
+def exportfilelist(fileList, name, path):
+    filepath = iteratefilename(os.path.join(path, name), prefix="_")
+    try:
+        file = open(filepath, 'x')
+    except:
+        print("SOMETHING IS WRONG THIS SHOULD NEVER BE REACHED.")
+        sys.exit()
+
+    for ele in fileList:
+        file.write(ele + "\n")
+
+    file.close()
+
+# notrim = do not trim, punctuation = dont remove punctuation, alpha = remove numbers, case = keep original case
+def formattext(text, notrim=False, punctuation=False, alpha=False, case=False, quiet=False, verbose=False):
+    if not notrim:
+        # Remove all whitespace and replace with singular space
+        #text.strip()
+        text = " ".join(text.split())
+    if not punctuation:
+        # Remove all punctuation
+        text = text.translate(str.maketrans('', '', string.punctuation))
+    if alpha:
+        # Remove all numbers
+        text = "".join(i for i in text if not i.isdigit())
+    if not case:
+        # Convert all letters to lowercase
+        text = text.lower()
+    return text
+
+def gettext(filepath):
+    #text = ""
+    with open(filepath, "r") as document:
+        text = document.readlines()
+    return text
+
+def main():
+    parser = argparse.ArgumentParser(description="Do preprocessing on text data for supervised learning tasks. Take text files from source directories and perform preprocessing operation on each document. By default excess whitespace from text is removed (leading and trailing whitespace is removed, and all whitespace between words is reduced to a singular space), all letters are converted to lowercase, and all punctuation is removed. The directory tree determines the data labels such that each folder name is a label for all texts held within itself and its subdirectories. Whitespace in labels will be replaced with underscores. Export each text as a flat file to the target directory along with a single metadata file 'metadata.csv' holding the labels, text properties, and the path to the flattened text file. Texts that are unlabeled will have their paths exported to a file 'unlabeled.csv' in the same directory as the metadata file. Texts that contain useless data will have their paths exported to a file 'useless.csv' in the same directory as the metadata file.")
+    parser.add_argument("target", type=str, help="Path to the target directory where all processed data output files will be stored.")
+    parser.add_argument("-m", "--metadata", type=str, help="Optional path to the metadata file. If unset then metadata file will be stored in the target directory.")
+    parser.add_argument("-s", "--source", action="append", type=str, help="Path to the source directory holding text files to be processed. The folder names of the directory tree determines the labels for each text. Each extra argument will add another source directory to the list of directories.")
+    parser.add_argument("-r", "--root", action="store_true", help="Do not ignore the root directory name in the labeling process for each source file.")
+    parser.add_argument("-c", "--compress", action="store_true", help="Compress each output file using GNU zip (.gz).")
+    parser.add_argument("-o", "--override", action="store_true", help="Allows for the target directory to be an existing directory and will override any existing files if collision occurs during export.")
+    parser.add_argument("-a", "--abspath", action="store_true", help="Force absolute path names.")
+    parser.add_argument("-t", "--notrim", action="store_true", help="Do not trim text such that all original whitespace is kept.")
+    parser.add_argument("-p", "--punctuation", action="store_true", help="Keep original punctuation in document; do not remove punctuation.")
+    parser.add_argument("-n", "--alpha", action="store_true", help="Remove all numbers from text.")
+    parser.add_argument("-k", "--case", action="store_true", help="Keep original letter case in document; do not convert text to all lowercase letters.")
+    parser.add_argument("-i", "--minchars", type=int, default=0, help="Optional flag setting the minimum characters allowed for text to be considered useful data.")
+    parser.add_argument("-f", "--maxchars", type=int, default=0, help="Optional flag setting the maximum characters allowed for text to be considered useful data.")
+    log_group = parser.add_mutually_exclusive_group()
+    log_group.add_argument("-v", "--verbose", action="store_true", help="Output actions to the console and show detailed information.")
+    log_group.add_argument("-q", "--quiet", action="store_true", help="Suppress ouptut to the console.")
+
+    args = parser.parse_args()
+
+    if createdir(args.target, label="target", clean=(not args.override), verbose=args.verbose, quiet=args.quiet):
+        sys.exit()
+    if checkdirs(args.source, label="source", verbose=args.verbose, quiet=args.quiet):
+        sys.exit()
+    if args.metadata:
+        if createdir(args.metadata, label="metadata", clean=False, verbose=args.verbose, quiet=args.quiet):
+            sys.exit()
+
+    if not args.quiet:
+        print("Starting operation...")
+
+    data = []
+    unlabeled = []
+    useless = []
+    i = 0
+    for source in args.source:
+        for root, dirs, files in os.walk(source):
+            for file in files:
+                filepath = os.path.join(root, file)
+                if args.abspath:
+                    filepath = os.path.abspath(filepath)
+                if not args.quiet:
+                    print("Currently processing document: " + file)
+                elif args.verbose:
+                    print("Currently processing document: " + filepath)
+                labelpath = root
+                if not args.root:
+                    labelpath = removeaffix(root, prefix=source)
+                labels = getlabels(trimpathsep(labelpath), verbose=args.verbose)
+                # If data is labeled
+                if labels[0]:
+                    # Get document file ready as text
+                    text = gettext(filepath)
+                    # Format the text
+                    text = formattext(text, notrim=args.notrim, punctuation=args.punctuation, alpha=args.alpha, case=args.case, quiet=args.quiet, verbose=args.verbose)
+                    # Generate metadata
+                    meta = getmeta(text, verbose=args.verbose)
+
+                    if checkuseful(meta, minchars=args.minchars, maxchars=args.maxchars, verbose=args.verbose):
+                        # Save new formatted and flattend text
+                        flatpath = savetext(text, str(i), args.target, compress=args.compress, abspath=args.abspath)
+                        i += 1
+                        data.append([labels, meta, flatpath])
+                    else:
+                    useless.append(filepath)
+
+                # Else, data is not labeled
+                else:
+                    if args.verbose:
+                        print("Current text is unlabeled. Text will be ignored.")
+                    unlabeled.append(filepath)
+
+    if args.verbose:
+        print("Finished processing documents. Now exporting metadata...")
+
+    if args.metadata:
+        exportmeta(data, args.metadata, float=args.float)
+        exportfilelist(unlabeled, UNLABELED_FILENAME, args.metadata)
+        exportfilelist(useless, USELESS_FILENAME, args.metadata)
+    else:
+        exportmeta(data, args.target, float=args.float)
+        exportfilelist(unlabeled, UNLABELED_FILENAME, args.target)
+        exportfilelist(useless, USELESS_FILENAME, args.target)
+
+    if not args.quiet:
+        print("Finished operation.")
 
 
-# pdfTextMiner.py
-# Python 2.7.6
-# For Python 3.x use pdfminer3k module
-# This link has useful information on components of the program
-# https://euske.github.io/pdfminer/programming.html
-# http://denis.papathanasiou.org/posts/2010.08.04.post.html
 
-
-''' Important classes to remember
-PDFParser - fetches data from pdf file
-PDFDocument - stores data parsed by PDFParser
-PDFPageInterpreter - processes page contents from PDFDocument
-PDFDevice - translates processed information from PDFPageInterpreter to whatever you need
-PDFResourceManager - Stores shared resources such as fonts or images used by both PDFPageInterpreter and PDFDevice
-LAParams - A layout analyzer returns a LTPage object for each page in the PDF document
-PDFPageAggregator - Extract the decive to page aggregator to get LT object elements
-'''
-
-import os
-from pdfminer.pdfparser import PDFParser
-from pdfminer.pdfdocument import PDFDocument
-from pdfminer.pdfpage import PDFPage
-# From PDFInterpreter import both PDFResourceManager and PDFPageInterpreter
-from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
-from pdfminer.pdfdevice import PDFDevice
-# Import this to raise exception whenever text extraction from PDF is not allowed
-from pdfminer.pdfpage import PDFTextExtractionNotAllowed
-from pdfminer.layout import LAParams, LTTextBox, LTTextLine
-from pdfminer.converter import PDFPageAggregator
-
-''' This is what we are trying to do:
-1) Transfer information from PDF file to PDF document object. This is done using parser
-2) Open the PDF file
-3) Parse the file using PDFParser object
-4) Assign the parsed content to PDFDocument object
-5) Now the information in this PDFDocumet object has to be processed. For this we need
-   PDFPageInterpreter, PDFDevice and PDFResourceManager
- 6) Finally process the file page by page
-'''
-
-base_path = "C://some_folder"
-
-my_file = os.path.join(base_path + "/" + "test_pdf.pdf")
-log_file = os.path.join(base_path + "/" + "pdf_log.txt")
-
-password = ""
-extracted_text = ""
-
-# Open and read the pdf file in binary mode
-fp = open(my_file, "rb")
-
-# Create parser object to parse the pdf content
-parser = PDFParser(fp)
-
-# Store the parsed content in PDFDocument object
-document = PDFDocument(parser, password)
-
-# Check if document is extractable, if not abort
-if not document.is_extractable:
-	raise PDFTextExtractionNotAllowed
-
-# Create PDFResourceManager object that stores shared resources such as fonts or images
-rsrcmgr = PDFResourceManager()
-
-# set parameters for analysis
-laparams = LAParams()
-
-# Create a PDFDevice object which translates interpreted information into desired format
-# Device needs to be connected to resource manager to store shared resources
-# device = PDFDevice(rsrcmgr)
-# Extract the decive to page aggregator to get LT object elements
-device = PDFPageAggregator(rsrcmgr, laparams=laparams)
-
-# Create interpreter object to process page content from PDFDocument
-# Interpreter needs to be connected to resource manager for shared resources and device
-interpreter = PDFPageInterpreter(rsrcmgr, device)
-
-# Ok now that we have everything to process a pdf document, lets process it page by page
-for page in PDFPage.create_pages(document):
-	# As the interpreter processes the page stored in PDFDocument object
-	interpreter.process_page(page)
-	# The device renders the layout from interpreter
-	layout = device.get_result()
-	# Out of the many LT objects within layout, we are interested in LTTextBox and LTTextLine
-	for lt_obj in layout:
-		if isinstance(lt_obj, LTTextBox) or isinstance(lt_obj, LTTextLine):
-			extracted_text += lt_obj.get_text()
-
-#close the pdf file
-fp.close()
-
-# print (extracted_text.encode("utf-8"))
-
-with open(log_file, "wb") as my_log:
-	my_log.write(extracted_text.encode("utf-8"))
-print("Done !!")
+if __name__ == "__main__":
+    main()
